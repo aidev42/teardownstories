80fcd925ae5da5892f9d20c7c3ff7f95b04aa211
mattoconnor <matt.oconnor217@gmail.com>
2016-10-14 17:16:25 -0400

    added ability to create edit and delete posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,50 @@
+class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @posts = Post.all.order("created_at DESC")
+  end
+
+  def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_parameters)
+
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_parameters)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_parameters
+    params.require(:post).permit(:title, :link, :description)
+  end
+end
